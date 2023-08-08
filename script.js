73f239bc181cbf804d6eef3b2edb54daddeafe0f
wjdskagjs1 <wjdskagjs1@naver.com>
2023-08-08 18:15:45 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,82 @@
+const msgerForm = get(".msger-inputarea");
+const msgerInput = get(".msger-input");
+const msgerChat = get(".msger-chat");
+
+const BOT_MSGS = [
+	"그래.",
+	"맞아.",
+	"어.",
+	"아니.",
+	"안돼.",
+	"안.돼."
+];
+
+// Icons made by Freepik from www.flaticon.com
+const BOT_IMG = "sorabot.png";
+const PERSON_IMG = "default.png";
+const BOT_NAME = "마법의 소라고동";
+const PERSON_NAME = "당신";
+
+msgerForm.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const msgText = msgerInput.value;
+  if (!msgText) return;
+
+  appendMessage(PERSON_NAME, PERSON_IMG, "right", msgText);
+  msgerInput.value = "";
+
+  if(msgText.endsWith('?')){
+    botResponse();
+  }else{
+    botResponse('다시 한 번 물어봐.');
+  }
+});
+
+function appendMessage(name, img, side, text) {
+  //   Simple solution for small apps
+  const msgHTML = `
+    <div class="msg ${side}-msg">
+      <div class="msg-img" style="background-image: url(${img})"></div>
+
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${name}</div>
+          <div class="msg-info-time">${formatDate(new Date())}</div>
+        </div>
+
+        <div class="msg-text">${text}</div>
+      </div>
+    </div>
+  `;
+
+  msgerChat.insertAdjacentHTML("beforeend", msgHTML);
+  msgerChat.scrollTop += 500;
+}
+
+function botResponse(msg=null) {
+
+  const r = random(0, BOT_MSGS.length - 1);
+  const msgText = msg === null ? BOT_MSGS[r] : msg;
+  const delay = msgText.split(" ").length * 100;
+
+  setTimeout(() => {
+    appendMessage(BOT_NAME, BOT_IMG, "left", msgText);
+  }, delay);
+}
+
+// Utils
+function get(selector, root = document) {
+  return root.querySelector(selector);
+}
+
+function formatDate(date) {
+  const h = "0" + date.getHours();
+  const m = "0" + date.getMinutes();
+
+  return `${h.slice(-2)}:${m.slice(-2)}`;
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
